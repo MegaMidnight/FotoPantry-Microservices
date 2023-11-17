f742820990516bb5abf521129662d30b771ac245
Ryan <69286341+MegaMidnight@users.noreply.github.com>
2023-11-16 17:13:50 -0800

    Initial commit

---
+++ src/main/kotlin/com/fotopantry/fotopantrymediaupload/FotoPantryMediaUploadApplication.kt
@@ -0,0 +1,11 @@
+package com.fotopantry.fotopantrymediaupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FotoPantryMediaUploadApplication
+
+fun main(args: Array<String>) {
+    runApplication<FotoPantryMediaUploadApplication>(*args)
+}
---
+++ src/test/kotlin/com/fotopantry/fotopantrymediaupload/FotoPantryMediaUploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fotopantry.fotopantrymediaupload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FotoPantryMediaUploadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
